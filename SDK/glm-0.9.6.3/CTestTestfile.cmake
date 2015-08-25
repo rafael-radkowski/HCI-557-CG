ddcea9990cc73fc2e96e981981f55754a8c36434
Rafael Radkowski <rafael@iastate.edu>
2015-08-25 10:30:28 -0500

    Added ignore files

--- SDK/glm-0.9.6.3/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rafael/Documents/Teaching/ME557_2015/src/SDK/glm-0.9.6.3
-# Build directory: C:/Users/rafael/Documents/Teaching/ME557_2015/src/SDK/glm-0.9.6.3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(glm)
-SUBDIRS(test)
-SUBDIRS(util)
--- SDK/glm-0.9.6.3/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rafael/Documents/Teaching/ME557_2015/src/SDK/glm-0.9.6.3/util
-# Build directory: C:/Users/rafael/Documents/Teaching/ME557_2015/src/SDK/glm-0.9.6.3/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
